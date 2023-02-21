f78f0359d4cadee954eb4213af55e92c5d05ad5e
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2023-02-21 19:10:17 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+function mousesout(){
+    document.getElementById("pic1").src = "./Fabrics/jla230504_white_1.jpg" 
+    document.getElementById("pic2").src = "./Fabrics/jla230504_yellow_4.jpg"
+
+}
+
+function mouseshover(){
+        document.getElementById("pic1").src = "./Fabrics/jla230504_white_2.jpg"  
+        document.getElementById("pic2").src = "./Fabrics/jla230504_yellow_5.jpg"  
+
+}
